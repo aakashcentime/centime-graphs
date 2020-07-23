a8501e41b39e2d5dcab1315a549db5144f653060
Aakash Kedia <aakash@centime.com>
2020-07-23 05:49:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,22 @@ function App() {
 
   const [data, setData] = useState([12,23,25,34,45]);
   const [width , setWidth] = useState(500);
+  let N = 300;
+  let [someData, setSomeData] = useState([]);
+
+  function f(){
+      let tempArray1 = [];
+      for (let i = 0; i < N; i++) {
+      let currentDate = new Date();
+      currentDate.setDate(currentDate.getDate() + i);
+      console.log("i am here")
+      tempArray1.push({
+          date: currentDate,
+          value: Math.random(),
+          group: currentDate.getMonth()
+      });
+  }
+       setSomeData(tempArray1);}
 
   useEffect(()=>{
     console.log(svgRef);
@@ -27,17 +43,17 @@ function App() {
         numSpirals = 3
     let margin = {top:50,bottom:50,left:50,right:50};
 
-    var theta = function(r) {
+    let theta = function(r) {
       return numSpirals * Math.PI * r;
     };
     let colors = ["#5c9851","#5c9851","#5c9851","#5c9851","#BBBBBB"];
-    var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"]
+    let months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"]
     // used to assign nodes color by group
-    var color = d3.scaleOrdinal(d3.schemeCategory10);
+    let color = d3.scaleOrdinal(d3.schemeCategory10);
 
-    var r = d3.min([width, height]) / 2 - 40;
+    let r = d3.min([width, height]) / 2 - 40;
 
-    var radius = d3.scaleLinear()
+    let radius = d3.scaleLinear()
         .domain([start, end])
         .range([40, r]);
 
@@ -46,44 +62,46 @@ function App() {
         .attr("height", height + margin.left + margin.right)
         .append("g")
         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+      svg.selectAll("*").remove();
 
-    var points = d3.range(start, end + 0.001, (end - start) / 1000);
+    let points = d3.range(start, end + 0.001, (end - start) / 1000);
 
-    var spiral = d3.radialLine()
+    let spiral = d3.radialLine()
         .curve(d3.curveCardinal)
         .angle(theta)
         .radius(radius);
 
-    var path = svg.append("path")
+    let path = svg.append("path")
         .datum(points)
         .attr("id", "spiral")
         .attr("d", spiral)
         .style("fill", "none")
         .style("stroke", "steelblue");
 
-    var spiralLength = path.node().getTotalLength(),
-        N = 365,
+    let spiralLength = path.node().getTotalLength(),
         barWidth = (spiralLength / N) - 1;
-    var someData = [];
-    for (var i = 0; i < N; i++) {
-      var currentDate = new Date("2020-01-01");
+    let tempArray1 =[];
+    for (let i = 0; i < N; i++) {
+      let currentDate = new Date();
       currentDate.setDate(currentDate.getDate() + i);
-      console.log(currentDate.getMonth())
-      someData.push({
+      console.log("i am here")
+        tempArray1.push({
         date: currentDate,
         value: Math.random(),
         group: currentDate.getMonth()
       });
     }
+   // setSomeData(tempArray1);
 
-    var timeScale = d3.scaleTime()
+
+    let timeScale = d3.scaleTime()
         .domain(d3.extent(someData, function(d){
           return d.date;
         }))
         .range([0, spiralLength]);
 
     // yScale for the bar height
-    var yScale = d3.scaleLinear()
+    let yScale = d3.scaleLinear()
         .domain([0, d3.max(someData, function(d){
           return d.value;
         })])
@@ -95,7 +113,7 @@ function App() {
         .append("rect")
         .attr("x", function(d,i){
 
-          var linePer = timeScale(d.date),
+          let linePer = timeScale(d.date),
               posOnLine = path.node().getPointAtLength(linePer),
               angleOnLine = path.node().getPointAtLength(linePer - barWidth);
 
@@ -119,15 +137,18 @@ function App() {
         .style("fill", function(d){
           console.log(d.date.getMonth());
           let rand = d.date.getMonth()%colors.length;
+          // if(d.value%2==0){
+          //     console.log("at line")
+          //     return "#FFAD113E"}
           // return color(d.group);})
-          return colors[rand]})
+            return "#4a8d0f"})
         .style("stroke", "none")
         .attr("transform", function(d){
           return "rotate(" + d.a + "," + d.x  + "," + d.y + ")"; // rotate the bar
         });
 
     // add date labels
-    var tF = d3.timeFormat("%b %Y"),
+    let tF = d3.timeFormat("%b %Y"),
         firstInMonth = {};
 
     svg.selectAll("text")
@@ -140,8 +161,8 @@ function App() {
         .append("textPath")
         // only add for the first of each month
         .filter(function(d){
-          var sd = tF(d.date);
-          // var sd = d.date.getMonth()
+          let sd = tF(d.date);
+          // let sd = d.date.getMonth()
           if (!firstInMonth[sd]){
             firstInMonth[sd] = 1;
             return true;
@@ -160,48 +181,72 @@ function App() {
         })
 
 
-    var tooltip = d3.select("#chart")
-        .append('div')
-        .attr('class', 'tooltip');
+    // let tooltip = d3.select("#chart")
+    //     .append('div')
+    //     .attr('class', 'tooltip');
+    //
+    // tooltip.append('div')
+    //     .attr('class', 'date');
+    // tooltip.append('div')
+    //     .attr('class', 'value');
+    //
+    // svg.selectAll("rect")
+    //     .on('mouseover', function(d) {
+    //
+    //       tooltip.select('.date').html("Date: <b>" + d.date.toDateString() + "</b>");
+    //       tooltip.select('.value').html("Value: <b>" + Math.round(d.value*100)/100 + "<b>");
+    //
+    //       d3.select(this)
+    //           .style("fill","#FFFFFF")
+    //           .style("stroke","#000000")
+    //           .style("stroke-width","2px");
+    //
+    //       tooltip.style('display', 'block');
+    //       tooltip.style('opacity',2);
+    //
+    //     })
+    //     .on('mousemove', function(d) {
+    //       tooltip.style('top', (d3.event.layerY + 10) + 'px')
+    //           .style('left', (d3.event.layerX - 25) + 'px');
+    //     })
+    //     .on('mouseout', function(d) {
+    //       d3.selectAll("rect")
+    //           .style("fill", function(d){return color(d.group);})
+    //           .style("stroke", "none")
+    //
+    //       tooltip.style('display', 'none');
+    //       tooltip.style('opacity',0);
+    //     });
 
-    tooltip.append('div')
-        .attr('class', 'date');
-    tooltip.append('div')
-        .attr('class', 'value');
+  },[someData]);
 
-    svg.selectAll("rect")
-        .on('mouseover', function(d) {
+    const svgRef = useRef();
 
-          tooltip.select('.date').html("Date: <b>" + d.date.toDateString() + "</b>");
-          tooltip.select('.value').html("Value: <b>" + Math.round(d.value*100)/100 + "<b>");
+    const loadingFunction = () =>{
+        return [{"success":true,"height":500},{"success":false,"height":600}]
+    }
+    const myfunc = () => {
 
-          d3.select(this)
-              .style("fill","#FFFFFF")
-              .style("stroke","#000000")
-              .style("stroke-width","2px");
+        document.getElementById('chart').innerHTML="";
+        d3.selectAll("svg > *").remove();
+        const months = [1,2,3,300];
 
-          tooltip.style('display', 'block');
-          tooltip.style('opacity',2);
-
-        })
-        .on('mousemove', function(d) {
-          tooltip.style('top', (d3.event.layerY + 10) + 'px')
-              .style('left', (d3.event.layerX - 25) + 'px');
-        })
-        .on('mouseout', function(d) {
-          d3.selectAll("rect")
-              .style("fill", function(d){return color(d.group);})
-              .style("stroke", "none")
-
-          tooltip.style('display', 'none');
-          tooltip.style('opacity',0);
-        });
-
-  },[data]);
-
-  const svgRef = useRef();
-
-  return (<div id="chart"></div>);
+         N = months[Math.floor(Math.random() * months.length)];
+        let tempArray =[];
+        for (let i = 0; i < N; i++) {
+            let currentDate = new Date();
+            currentDate.setDate(currentDate.getDate() + i);
+            console.log("in button")
+            tempArray.push({
+                date: currentDate,
+                value: Math.random(),
+                group: currentDate.getMonth()
+            });
+        }
+        setSomeData(tempArray);
+        
+    };
+  return (<div><div id="chart"></div><button onClick={myfunc}>hello</button></div> );
 }
 
 export default App;
